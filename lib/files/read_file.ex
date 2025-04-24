af869d1b0c58f4a9c634fcc1c42b2ca5fbb1503d
belgoros <s.cambour@gmail.com>
2025-04-24 16:27:22 +0200

    Add example of reading a CSV file

--- lib/files/read_file.ex
+++ lib/files/read_file.ex
@@ -21,4 +21,18 @@ defmodule Files.ReadFile do
 		{:ok, csv_data} = File.read(@todos_file)
 		csv_data
 	end
+
+	def read_csv(file_path) do
+	    file_path
+	    |> File.stream!()
+	    |> Stream.drop(1) # Skip the header
+	    |> Stream.map(&parse_line/1)
+	    |> Enum.to_list()
+	  end
+
+	  defp parse_line(line) do
+	    line
+	    |> String.trim()     # Remove newline characters
+	    |> String.split(";") # Split by semicolon
+	  end
 end
