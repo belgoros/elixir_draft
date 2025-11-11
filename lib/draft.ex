3f5c6be9380d6f851606d776c9d48a9c50ed3d8c
belgoros <s.cambour@gmail.com>
2025-11-11 11:39:24 +0100

    Add an example of using Pry for debugging

--- lib/draft.ex
+++ lib/draft.ex
@@ -1,8 +1,14 @@
 defmodule ElixirDraft.Draft do
-  require IEx
+  def print() do
+    guess = "1"
+    number_to_guess = 10
 
-  def print(state \\ %{}) do
-    IEx.pry()
-    IO.puts("received state: #{inspect(state)}")
+    message =
+      case guess do
+        ^number_to_guess -> "yay, you win with the correct answer of: #{number_to_guess}"
+        _ -> "Your guess: #{guess}. Wrong. Guess again. "
+      end
+
+    IO.puts(message)
   end
 end
